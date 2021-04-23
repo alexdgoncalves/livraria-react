ae3752515ec343b86d44d5427179ecf2d57d0acf
alexdgoncalves <alex.d.goncalves@21@gmail.com>
2021-04-22 21:52:23 -0300

    fazendo uso dos hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import { useState } from "react";
+import Routes from "./routes";
 
 function App() {
-  // useState é um array
-  // primeiro parametro é a var que sera atribuido/ alterado o valor
-  // ja o segundo é uma função, ela a qual deve ser chamada para fazer a alteração do primeiro parametro
-  const [contador, setContador] = useState(0);
-  function increment() {
-    console.log("somando 1");
-    setContador(contador + 1);
-  }
-  function remove() {
-    console.log("removendo");
-    setContador(contador - 1);
-  }
-  return (
-    <div className="App">
-      <p>contagem: {contador}</p>
-      <button onClick={increment}>adicionar 1</button>
-      <button onClick={remove}>remover 1</button>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
