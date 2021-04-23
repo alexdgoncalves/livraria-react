9eb93d1c90e2a73008e0be80235772503220dd1e
alexdgoncalves <alex.d.goncalves@21@gmail.com>
2021-04-22 21:10:49 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 function App() {
+  // useState é um array
+  // primeiro parametro é a var que sera atribuido/ alterado o valor
+  // ja o segundo é uma função, ela a qual deve ser chamada para fazer a alteração do primeiro parametro
+  const [contador, setContador] = useState(0);
+  function increment() {
+    console.log("somando 1");
+    setContador(contador + 1);
+  }
+  function remove() {
+    console.log("removendo");
+    setContador(contador - 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <p>contagem: {contador}</p>
+      <button onClick={increment}>adicionar 1</button>
+      <button onClick={remove}>remover 1</button>
     </div>
   );
 }
